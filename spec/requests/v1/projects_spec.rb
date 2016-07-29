4990c94b6e85dd208c211269891cfbd0288744ce
Marco Damaceno <maadmaaceno@gmail.com>
2016-07-29 13:58:16 -0300

    --allow-empty-message

---
+++ spec/requests/v1/projects_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Project requests', type: :request do
+  it 'sends a list of projects'
+  it 'retrieves a specific project'
+  it 'saves a new project'
+  it 'updates a project'
+  it 'deletes a project'
+end
---
+++ spec/requests/v1/workers_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Workers requests', type: :request do
+  it 'sends a list of workers'
+  it 'retrieves a specific worker'
+  it 'saves a new worker'
+  it 'updates a worker'
+  it 'deletes a worker'
+end
